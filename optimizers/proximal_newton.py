22d736d09a9a833fadf82f83b2b8dfef00661f1a
Ninomiya Lab <92347908+ninomiyalab@users.noreply.github.com>
2026-01-04 11:34:29 +0900

    Update proximal_newton.py
    s

--- optimizers/proximal_newton.py
+++ optimizers/proximal_newton.py
@@ -1,8 +1,8 @@
 import numpy as np
 from scipy.linalg import lu_factor, lu_solve
 
-def proximal_newton(f, jac, hess, w_init, alpha=1.0, eps=1.0, max_iter=100, tol=1e-6):
-    """Proximal (Regularized) Newton Method"""
+def diagonal_newton(f, jac, hess, w_init, alpha=1.0, eps=1.0, max_iter=100, tol=1e-6):
+    """Diagonal (Proximal) Newton Method"""
     w = np.array(w_init, dtype=float)
     identity = np.identity(len(w))
     history = [f(w)]
