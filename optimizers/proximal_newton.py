c9de33da2a330f284f1d59cc3d04ff5da15517cc
Ninomiya Lab <92347908+ninomiyalab@users.noreply.github.com>
2026-01-04 09:49:39 +0900

    Create proximal_newton.py
    P

---
+++ optimizers/proximal_newton.py
@@ -0,0 +1,17 @@
+import numpy as np
+from scipy.linalg import lu_factor, lu_solve
+
+def proximal_newton(f, jac, hess, w_init, alpha=1.0, eps=1.0, max_iter=100, tol=1e-6):
+    """Proximal (Regularized) Newton Method"""
+    w = np.array(w_init, dtype=float)
+    identity = np.identity(len(w))
+    history = [f(w)]
+    for ite in range(max_iter):
+        try:
+            lu, piv = lu_factor(hess(w) + eps * identity)
+            delta_x = lu_solve((lu, piv), -jac(w))
+            w += alpha * delta_x
+            history.append(f(w))
+            if np.linalg.norm(delta_x) < tol: break
+        except np.linalg.LinAlgError: break
+    return {"x": w, "history": history, "iterations": ite + 1}
